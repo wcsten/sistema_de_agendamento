c61b26b5c439163cdbb4af2395aa927104a44f20
wcsten <gabriel_sten@hotmail.com>
2019-05-06 15:18:44 -0300

    Added STATIC_ROOT in settings

--- sistema_de_agendamento/settings.py
+++ sistema_de_agendamento/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
